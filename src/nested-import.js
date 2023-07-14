1de7ec0e9a473e472a2afbb941e68d9c8f390a6a
Bill Harding <wbharding@gmail.com>
2023-07-13 18:34:49 -0700

    Plugin tests passing or so; avoid re-processing files

--- src/nested-import.js
+++ src/nested-import.js
@@ -1,5 +1,5 @@
 import { fetchWithRetry } from "lib/plugin-import-inliner"
 
-export function wrappedFetch(url, options) {
+export async function wrappedFetch(url, options) {
   return fetchWithRetry(url, options)
 }
