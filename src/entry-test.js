f6ac19a3f56d5c64d64f3a69da89dd9afc937305
Lucian Ruscanu <lucianruscanu@gmail.com>
2023-10-04 18:11:33 +0300

    Test: add compatibility with separate imported functions

--- src/entry-test.js
+++ src/entry-test.js
@@ -1,6 +1,5 @@
 import { inlineImportsFromGithub } from "lib/plugin-import-inliner"
 import { wrappedFetch } from "./nested-import"
-import { followedBySemi, generator } from "./fancy-function-test.js"
 
 const plugin = {
   insertText: async function(app) {
